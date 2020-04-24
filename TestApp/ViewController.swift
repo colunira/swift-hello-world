aee5c74af07812ff12b09ed1898fd62561335928
Julia Iskierka <jul.iskierka@gmail.com>
2020-04-24 10:07:29 +0200

    Create an app

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func Clicked(_ sender: Any) {
+        counter += 1
+        label.text = "Button clicked \(counter) times"
+    }
 }
 
